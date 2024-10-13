efdcd87b92f7d35365c4c1c8b2963779ad160430
haluongtan <160831939+haluongtan@users.noreply.github.com>
2024-10-13 14:14:51 +0700

    Add files via upload

---
+++ StudentBUS.cs
@@ -0,0 +1,59 @@
+﻿using DAL4.entities;
+using DAL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS4
+{
+    public class StudentBUS
+    {
+        private StudentDAL studentDAL;
+
+        public StudentBUS()
+        {
+            studentDAL = new StudentDAL();
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return studentDAL.GetAllStudents();
+        }
+        public List<Student> GetAllStudentsWithNoMajor()
+        {
+            return studentDAL.GetAllStudentsWithNoMajor();
+        }
+        public List<Faculty> GetAllFaculties()
+        {
+            return studentDAL.GetAllFaculties();
+        }
+
+        public List<Major> GetAllMajors()
+        {
+            return studentDAL.GetAllMajors();
+        }
+        public Student GetStudentById(string studentID)
+        {
+            return studentDAL.GetStudentById(studentID);
+        }
+
+        public void AddOrUpdateStudent(Student student)
+        {
+            var existingStudent = studentDAL.GetStudentById(student.StudentID);
+            if (existingStudent == null)
+            {
+                studentDAL.AddStudent(student);
+            }
+            else
+            {
+                studentDAL.UpdateStudent(student);
+            }
+        }
+        public void DeleteStudent(string studentID)
+        {
+            studentDAL.DeleteStudent(studentID);
+        }
+    }
+}
---
+++ StudentDAL.cs
@@ -0,0 +1,70 @@
+﻿using DAL4.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL4
+{
+
+    public class StudentDAL
+    {
+        private QuanLySinhVienModel db;
+        public StudentDAL()
+        {
+            db = new QuanLySinhVienModel();
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return db.Student.Include("Faculty").Include("Major").ToList();
+        }
+        public List<Student> GetAllStudentsWithNoMajor()
+        {
+            return db.Student.Include("Faculty").Include("Major").Where(s => s.MajorID == null).ToList();
+        }
+        public List<Faculty> GetAllFaculties()
+        {
+            return db.Faculty.ToList();
+        }
+
+        public List<Major> GetAllMajors()
+        {
+            return db.Major.ToList();
+        }
+        public Student GetStudentById(string studentID)
+        {
+            return db.Student.SingleOrDefault(s => s.StudentID == studentID);
+        }
+
+        public void AddStudent(Student student)
+        {
+            db.Student.Add(student);
+            db.SaveChanges();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            var existingStudent = db.Student.SingleOrDefault(s => s.StudentID == student.StudentID);
+            if (existingStudent != null)
+            {
+                existingStudent.FullName = student.FullName;
+                existingStudent.AverageScore = student.AverageScore;
+                existingStudent.FacultyID = student.FacultyID;
+                existingStudent.MajorID = student.MajorID;
+                existingStudent.Avatar = student.Avatar;
+                db.SaveChanges();
+            }
+        }
+        public void DeleteStudent(string studentID)
+        {
+            var student = db.Student.SingleOrDefault(s => s.StudentID == studentID);
+            if (student != null)
+            {
+                db.Student.Remove(student);
+                db.SaveChanges();
+            }
+        }
+    }
+}
